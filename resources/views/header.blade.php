ac70b1ad70da5530c10fb91aee0849a68f463fd7
damirusso95 <damirusso95@gmail.com>
2024-07-01 17:29:23 +0200

    M2: metodo per visualizzare la rout corrente

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,5 @@
 <header>
+    <span>attualmente sei in: {{ Route::currentRouteName() }}</span>
     <nav>
         <a href="{{ route("home") }}">HOME</a>
         <a href="{{ route("about") }}">ABOUT</a>
